4779cd8eb475e3140a909cfe39875da404e02d88
Justin Johnson <justinaugustejohnson@gmail.com>
2025-10-16 02:41:37 -0400

    yay displays

--- firmware/idf/components/audio_board/audio_board_stub.c
+++ firmware/idf/components/audio_board/audio_board_stub.c
@@ -27,15 +27,3 @@ esp_err_t audio_board_deinit(audio_board_handle_t audio_board)
     ESP_LOGI(TAG, "Audio board stub deinitialized");
     return ESP_OK;
 }
-
-esp_err_t audio_board_sdcard_init(esp_periph_set_handle_t set, periph_sdcard_mode_t mode)
-{
-    ESP_LOGW(TAG, "SD card not supported in stub implementation");
-    return ESP_ERR_NOT_SUPPORTED;
-}
-
-audio_hal_handle_t audio_board_get_hal(void)
-{
-    ESP_LOGW(TAG, "Audio HAL not supported in stub implementation");
-    return NULL;
-}
